e0d4337735924dfac38e6095a607be78dc946f80
Madara P <53658768+madcat13@users.noreply.github.com>
2021-03-06 12:14:58 +0000

    Add files via upload

---
+++ sql_scripts/insert_activities.sql
@@ -0,0 +1,31 @@
+
+-- Database: `travel` activities
+
+INSERT INTO `activities` (`activityID`, `activity_name`, `description`, `price`,
+`location`) VALUES
+(1, 'City Tour', 'Guided sightseening walk around Reykjavik', 1582.02, 'Reykjavik');
+
+
+INSERT INTO `activities` (`activityID`, `activity_name`, `description`, `price`,
+`location`) VALUES
+(2, 'Whale Watching', 'Boat trip into the wild sea', 1989.99, 'Akureyri');
+
+
+INSERT INTO `activities` (`activityID`, `activity_name`, `description`, `price`,
+`location`) VALUES
+(3, 'Discover Geysers', 'Visit the famous Strokkur and the Great Geysir', 1790.99, 'Haukadalur');
+
+
+INSERT INTO `activities` (`activityID`, `activity_name`, `description`, `price`,
+`location`) VALUES
+(4, 'Northern Lights', 'Northern Lights Cruise', 1999.99, 'Elding');
+
+
+INSERT INTO `activities` (`activityID`, `activity_name`, `description`, `price`,
+`location`) VALUES
+(5, 'Hot Spring Spa', 'Relax in natural hot spring', 2500.99, 'Strokkur');
+
+
+INSERT INTO `activities` (`activityID`, `activity_name`, `description`, `price`,
+`location`) VALUES
+(6, 'Icelandic Volcano Experience', 'Visit active volcano Eyjafjallajokull', 3080.99, 'Skogar ');
